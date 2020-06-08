0fba0456950d0f502cd98de47d3467e85901b149
Kendra Ryan <yesthisiskendra@gmail.com>
2020-06-08 12:40:06 -0700

    Start of using local storage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import useQuestions from "./hooks/useQuestions";
 import "./App.css";
 
 function App() {
+  const [questions, setQuestionCompleted] = useQuestions();
+
   return (
     <div className="App">
       <h1>Proust Prompts</h1>
+      {questions.map((question) => (
+        <div className="question">
+          <p>{question.question}</p>
+          <input
+            type="checkbox"
+            checked={question.completed}
+            onChange={() =>
+              setQuestionCompleted(question.id, !question.completed)
+            }
+          />
+        </div>
+      ))}
     </div>
   );
 }
---
+++ src/questions.js
@@ -0,0 +1,38 @@
+const questions = [
+  {
+    id: 1,
+    question: "What is your idea of perfect happiness?",
+    completed: true,
+  },
+  { id: 2, question: "What is your greatest fear?", completed: false },
+  {
+    id: 3,
+    question: "What is the trait you most deplore in yourself?",
+    completed: false,
+  },
+  {
+    id: 4,
+    question: "What is the trait you most deplore in others?",
+    completed: true,
+  },
+  {
+    id: 5,
+    question: "Which living person do you most admire?",
+    completed: false,
+  },
+  { id: 6, question: "What is your greatest extravagance?", completed: false },
+  { id: 7, question: "What is your current state of mind?", completed: false },
+  {
+    id: 8,
+    question: "What do you consider the most overrated virtue?",
+    completed: false,
+  },
+  { id: 9, question: "On what occasion do you lie?", completed: false },
+  {
+    id: 10,
+    question: "What do you most dislike about your appearance?",
+    completed: false,
+  },
+];
+
+export default questions;
