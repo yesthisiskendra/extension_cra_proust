f43d4cf1085fca5bbdea4e1b162567391c8d2801
Kendra Ryan <yesthisiskendra@gmail.com>
2020-06-08 13:13:52 -0700

    Moved useState to separate file to prepare for what's coming next

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import useQuestions from "./hooks/useQuestions";
 import "./App.css";
 
--- src/hooks/useQuestions.js
+++ src/hooks/useQuestions.js
@@ -1,21 +1,8 @@
 import { useState } from "react";
 import startingQuestions from "../questions";
 
-// const getQuestions = () => {
-//   return questions;
-//   const localQuestions = window.localStorage.getItem("localQuestions");
-//   if (!localQuestions) {
-//     return questions;
-//   }
-//   return JSON.parse(questions);
-// };
 const useQuestions = () => {
-  const [questions, setTodos] = useState(startingQuestions);
-  const updateQuestions = (newTodos) => {
-    const stringifiedTodos = JSON.stringify(newTodos);
-    window.localStorage.setItem("todos", stringifiedTodos);
-    setTodos(newTodos);
-  };
+  const [questions, updateQuestions] = useState(startingQuestions);
   const setQuestionCompleted = (id, completed) => {
     const questionIdx = questions.findIndex((question) => question.id === id);
     const newQuestions = [...questions];
